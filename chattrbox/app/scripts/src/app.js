0bb77ba553c96c752d3b9bb5f37ea4ee37524efb
Thomas Ngo <tngo0508@gmail.com>
2018-04-16 13:51:07 -0700

    finish chapter 17

--- chattrbox/app/scripts/src/app.js
+++ chattrbox/app/scripts/src/app.js
@@ -1,6 +1,17 @@
+import socket from "./ws-client";
+
 class ChatApp {
   constructor() {
-    console.log("Hellow ES6!");
+    socket.init("ws://localhost:3001");
+    socket.registerOpenHandler(() => {
+      let message = new ChatMessage({
+        message: "pow!"
+      });
+      socket.sendMessage(message.serialize());
+      socket.registerMessageHandler((data) => {
+        console.log(data);
+      });
+    });
   }
 }
 
