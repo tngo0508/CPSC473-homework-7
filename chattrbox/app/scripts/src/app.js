8bfa95cc6d149a4dbef121b0072eff63a30cb971
Thomas Ngo <tngo0508@gmail.com>
2018-04-16 15:56:55 -0700

    Connecting ChatForm to the socket

--- chattrbox/app/scripts/src/app.js
+++ chattrbox/app/scripts/src/app.js
@@ -1,5 +1,7 @@
 import socket from "./ws-client";
-import {ChatForm} from "./dom";
+import {
+  ChatForm
+} from "./dom";
 
 const FORM_SELECTOR = "[data-chat='chat-form']";
 const INPUT_SELECTOR = "[data-chat='message-input']";
@@ -7,16 +9,16 @@ const INPUT_SELECTOR = "[data-chat='message-input']";
 class ChatApp {
   constructor() {
     this.chatForm = new ChatForm(FORM_SELECTOR, INPUT_SELECTOR);
-    
+
     socket.init("ws://localhost:3001");
     socket.registerOpenHandler(() => {
-      let message = new ChatMessage({
-        message: "pow!"
+      this.chatForm.init((data) => {
+        let message = new ChatMessage(data);
+        socket.sendMessage(message.serialize());
       });
-      socket.sendMessage(message.serialize());
-      socket.registerMessageHandler((data) => {
-        console.log(data);
-      });
+    });
+    socket.registerMessageHandler((data) => {
+      console.log(data);
     });
   }
 }
