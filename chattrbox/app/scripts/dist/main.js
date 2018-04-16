692769443a95fb0edce209f01ffbfc90ddeb1d5c
Thomas Ngo <tngo0508@gmail.com>
2018-04-16 12:16:03 -0700

    first steps with babel

---
+++ chattrbox/app/scripts/dist/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var ChatApp = function ChatApp() {
+  _classCallCheck(this, ChatApp);
+
+  console.log("Hellow ES6!");
+};
+
+new ChatApp();
