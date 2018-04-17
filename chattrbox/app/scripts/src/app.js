2217ee65a228638287becf70155a810ee03abb28
Thomas Ngo <tngo0508@gmail.com>
2018-04-17 10:10:06 -0700

    confirm match output figure 18.2 except user is batman

--- chattrbox/app/scripts/src/app.js
+++ chattrbox/app/scripts/src/app.js
@@ -1,24 +1,29 @@
 import socket from "./ws-client";
 import {
-  ChatForm
+  ChatForm,
+  ChatList
 } from "./dom";
 
 const FORM_SELECTOR = "[data-chat='chat-form']";
 const INPUT_SELECTOR = "[data-chat='message-input']";
+const LIST_SELECTOR = "[data-chat='message-list']";
 
 class ChatApp {
   constructor() {
     this.chatForm = new ChatForm(FORM_SELECTOR, INPUT_SELECTOR);
+    this.chatList = new ChatList(LIST_SELECTOR, "wonderwoman");
 
     socket.init("ws://localhost:3001");
     socket.registerOpenHandler(() => {
       this.chatForm.init((data) => {
-        let message = new ChatMessage(data);
+        let message = new ChatMessage({message: data});
         socket.sendMessage(message.serialize());
       });
     });
     socket.registerMessageHandler((data) => {
       console.log(data);
+      let message = new ChatMessage(data);
+      this.chatList.drawMessage(message.serialize());
     });
   }
 }
--- chattrbox/app/scripts/src/dom.js
+++ chattrbox/app/scripts/src/dom.js
@@ -17,3 +17,42 @@ export class ChatForm {
     this.$form.find("button").on("click", () => this.$form.submit());
   }
 }
+
+export class ChatList {
+  constructor(listSel, username) {
+    this.$list = $(listSel);
+    this.username = username;
+  }
+
+  drawMessage({user: u, timestamp: t, message: m}) {
+    let $messageRow = $("<li>", {
+      "class": "message-row"
+    });
+
+    if (this.username === u) {
+      $messageRow.addClass("me");
+    }
+
+    let $message = $("<p>");
+
+    $message.append($("<span>", {
+      "class": "message-username",
+      text: u
+    }));
+
+    $message.append($("<span>", {
+      "class": "timestamp",
+      "data-time": t,
+      text: (new Date(t)).getTime()
+    }));
+
+    $message.append($("<span>", {
+      "class": "message-message",
+      text: m
+    }));
+
+    $messageRow.append($message);
+    this.$list.append($messageRow);
+    $messageRow.get(0).scrollIntoView();
+  }
+}
